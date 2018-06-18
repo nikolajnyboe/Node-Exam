52abacd546ae5cb1b05527dacd982b446ceeb615
Nikolaj Nyboe <nikolaj@nyboe.dk>
2018-06-18 21:12:38 +0200

    updates to chat

--- handlers/websocket.js
+++ handlers/websocket.js
@@ -2,9 +2,8 @@ module.exports = io => {
   io.on('connect', socket => {
     console.log('User connected with id:', socket.id);
 
-    socket.on('buttonClick', message => {
-      socket.emit('clientMessage', message);
-      socket.broadcast.emit('clientMessage', message);
+    socket.on('message', message => {
+      io.emit('clientMessage', message);
     });
   });
 };
